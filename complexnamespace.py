75c6fa8ac2b4999517d2271c4170cc564e7555dd
Scott Colby <scolby33@gmail.com>
2019-01-14 13:15:08 -0500

    Major script revisions
    - split into functions
    - cache the TSV file
    - output to STDOUT or filename given as first (only) argument

--- complexnamespace.py
+++ complexnamespace.py
@@ -1,40 +1,105 @@
 import csv
+import os
 import sys
 from hashlib import sha256
-from typing import Optional
+from pathlib import Path
+from typing import Any, IO, Optional, Union
+from urllib.request import urlopen, URLError
+from warnings import warn
 
-from bel_resources import write_namespace
+from bel_resources import parse_bel_resource, write_namespace
 
-CHUNKSIZE = 1024
+CHUNKSIZE = 2 ** 20  # 1 megabyte
+
+NAMESPACE_URL = 'ftp://ftp.ebi.ac.uk/pub/databases/intact/complex/current/complextab/homo_sapiens.tsv'
+
+TSV_PATH = Path('homo_sapiens.tsv')
+
+NAMESPACE_NAME = 'Complex Portal'
+NAMESPACE_KEYWORD = 'complexportal'
+
+
+def retrieve_and_cache(uri: str, output_name: Union[str, Path]) -> str:
+    hasher = sha256()
+    with urlopen(uri) as remote_f:
+        try:
+            os.replace(output_name, f'{output_name}.bak')
+        except FileNotFoundError:
+            pass
+        with open(output_name, 'wb') as output_f:
+            chunk = remote_f.read(CHUNKSIZE)
+            while chunk:
+                hasher.update(chunk)
+                output_f.write(chunk)
+                chunk = remote_f.read(CHUNKSIZE)
+
+        return hasher.hexdigest()
+
+
+def compare_tsv_to_belns(path: Union[str, Path], digest: str) -> bool:
+    try:
+        with open(path, 'r') as belns:
+            resource = parse_bel_resource(belns)
+        if resource.get('Namespace', {}).get('VersionString') == digest:
+            return True
+    except FileNotFoundError:
+        pass
+
+    return False
+
+
+def tsv_to_belns(
+    tsv_path: Union[str, Path],
+    bel_path: Optional[Union[str, Path]],
+    digest: Optional[str] = None,
+) -> None:
+    with open(tsv_path, 'r') as tsv:
+        header = tsv.readline().lstrip('#')
+        fieldnames = header.split('\t')
+
+        reader = csv.DictReader(tsv, fieldnames=fieldnames, dialect='excel-tab')
+        complexnames = (row['Complex ac'] for row in reader)
+
+        try:
+            if bel_path is not None:
+                belns = open(bel_path, 'w')
+            else:
+                belns = None
+
+            write_namespace(
+                values={complexname: 'C' for complexname in complexnames},
+                namespace_name=NAMESPACE_NAME,
+                namespace_keyword=NAMESPACE_KEYWORD,
+                namespace_version=digest,
+                file=belns,
+            )
+        finally:
+            if belns is not None:
+                belns.close()
 
 
 def main() -> Optional[int]:
-    assert len(sys.argv) == 3, 'requires two arguments: input and output files'
+    assert (
+        len(sys.argv) == 1 or len(sys.argv) == 2
+    ), 'requires 0 or 1 arguments: [output filename]'
 
-    with open(sys.argv[1], 'rb') as tsv, open(sys.argv[2], 'w') as bel:
-        hasher = sha256()
-        chunk = tsv.read(CHUNKSIZE)
-        while chunk:
-            hasher.update(chunk)
-            chunk = tsv.read(CHUNKSIZE)
-        digest = hasher.hexdigest()
+    output_file = sys.argv[1] if len(sys.argv) == 2 else None
 
-        tsv.seek(0)
+    try:
+        digest = retrieve_and_cache(NAMESPACE_URL, TSV_PATH)
+    except URLError:
+        # TODO this breaks if the file can't be retrieved because digest isn't set
+        warn(f'could not retrieve `{NAMESPACE_URL}`, will try with cached version')
 
-        header = tsv.readline().decode('UTF-8').lstrip('#')
-        fieldnames = header.split('\t')
+    if output_file:
+        if compare_tsv_to_belns(output_file, digest):
+            print(f'`{NAMESPACE_URL}` has not changed; exiting', file=sys.stderr)
+            print(
+                f'delete or rename `{output_file}` to force a re-run', file=sys.stderr
+            )
+            return
 
-        tsv_decoded = (l.decode('UTF-8') for l in tsv)
-        reader = csv.DictReader(tsv_decoded, fieldnames=fieldnames, dialect='excel-tab')
-        complexnames = (row['Complex ac'] for row in reader)
-
-        write_namespace(
-            values={complexname: 'C' for complexname in complexnames},
-            namespace_name='Complex Portal',
-            namespace_keyword='complexportal',
-            namespace_version=digest,
-            file=bel,
-        )
+    tsv_to_belns(TSV_PATH, output_file, digest)
 
 
 if __name__ == '__main__':
